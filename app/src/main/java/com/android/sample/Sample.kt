74d88b3dcc87c5c9d893343d9f6f7c83d6821fc1
Angéline <angeline.bignens@epfl.ch>
2025-10-05 20:57:59 +0200

    feat: add Sample data class

---
+++ app/src/main/java/com/android/sample/Sample.kt
@@ -0,0 +1,12 @@
+package com.android.sample
+
+data class Sample(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val duration: String,
+    val tags: String,
+    val likes: Int,
+    val comments: Int,
+    val downloads: Int
+)
