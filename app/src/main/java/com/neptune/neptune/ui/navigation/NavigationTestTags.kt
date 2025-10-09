4a3736ed7581dad307146900031618d020ea1021
Tim <timeo.golaz@epfl.ch>
2025-10-09 22:12:28 +0200

    test(navigation): update test tags for navigation
    Renamed many tags and added TOP_BAR and TOP_BAR_TITLE constants to NavigationTestTags for improved clarity and expanded UI test coverage.

--- app/src/main/java/com/neptune/neptune/ui/navigation/NavigationTestTags.kt
+++ app/src/main/java/com/neptune/neptune/ui/navigation/NavigationTestTags.kt
@@ -3,7 +3,9 @@ package com.neptune.neptune.ui.navigation
 object NavigationTestTags {
   const val BOTTOM_NAVIGATION_MENU = "BottomNavigationMenu"
   const val GO_BACK_BUTTON = "GoBackButton"
-  const val MAIN_TAB = "OverviewTab"
-  const val EDIT_TAB = "MapTab"
-  const val PROFILE_TAB = "ProfileTab"
+  const val TOP_BAR = "TopBar"
+  const val TOP_BAR_TITLE = "TopBarTitle"
+  const val MAIN_TAB = "MainTab"
+  const val EDIT_TAB = "EditTab"
+  const val PROFILE_BUTTON = "ProfileButton"
 }
