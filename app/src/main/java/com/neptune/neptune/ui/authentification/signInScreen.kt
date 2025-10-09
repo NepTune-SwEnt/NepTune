76e52c3411f095246334e2235d700a6808a0930b
greg <gregory.blanc@epfl.ch>
2025-10-09 23:15:21 +0200

    feat: add sign-in screen with Google authentication
    Adds a new `SignInScreen.kt` composable for user authentication.
    The screen features a "Welcome" title and a "Sign in with Google" button.
    To include the Google logo, a new `google_logo.png` drawable is also added.

---
+++ app/src/main/java/com/neptune/neptune/ui/authentification/signInScreen.kt
@@ -0,0 +1,109 @@
+package com.neptune.neptune.ui.authentification
+
+import android.app.Activity
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.credentials.CredentialManager
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.neptune.neptune.ui.theme.DarkBlue1
+import com.neptune.neptune.ui.theme.LightTurquoise
+
+object SignInScreenTags {
+  const val LOGIN_TITLE = "loginTitle"
+  const val LOGIN_BUTTON = "loginButton"
+}
+
+@Composable
+fun SignInScreen(
+    /**
+     * You can add parameters with default values here, e.g., onClick: () -> Unit = {}, modifier:
+     * Modifier = Modifier, credentialManager: CredentialManager =
+     * CredentialManager.create(LocalContext.current)
+     *
+     * Note: Parameters with default values do not mean that you should use the default value in
+     * your implementation. They ensure that we can still use your code in our tests.
+     */
+    context: Context = LocalContext.current,
+    credentialManager: CredentialManager = CredentialManager.create(context),
+    navigateMain: () -> Unit = {},
+    signInViewModel: SignInViewModel = viewModel(),
+) {
+  LaunchedEffect(credentialManager) { signInViewModel.initialize(credentialManager, navigateMain) }
+  val googleId = com.neptune.neptune.R.drawable.google_logo
+
+  Surface(modifier = Modifier.fillMaxSize(), color = DarkBlue1) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+          Text(
+              text = "Welcome",
+              textAlign = TextAlign.Center,
+              style =
+                  TextStyle(
+                      fontSize = 80.sp,
+                      fontFamily = FontFamily(Font(com.neptune.neptune.R.font.markazi_text)),
+                      fontWeight = FontWeight(400)),
+              color = LightTurquoise,
+              modifier = Modifier.testTag(SignInScreenTags.LOGIN_TITLE))
+
+          Spacer(modifier = Modifier.height(200.dp))
+
+          ElevatedButton(
+              onClick = { signInViewModel.beginSignIn(context as Activity) },
+              colors =
+                  ButtonDefaults.buttonColors(
+                      containerColor = LightTurquoise,
+                      contentColor = MaterialTheme.colorScheme.onBackground),
+              shape = RoundedCornerShape(12.dp),
+              modifier = Modifier.testTag(SignInScreenTags.LOGIN_BUTTON)) {
+                Row {
+                  Image(
+                      painter = painterResource(id = googleId),
+                      contentDescription = "Google Logo",
+                      modifier = Modifier.size(30.dp).align(Alignment.CenterVertically))
+                  Spacer(modifier = Modifier.width(24.dp))
+                  Text(
+                      text = "Sign in with Google",
+                      style =
+                          TextStyle(
+                              fontSize = 30.sp,
+                              fontFamily =
+                                  FontFamily(Font(com.neptune.neptune.R.font.markazi_text)),
+                              fontWeight = FontWeight(400)),
+                  )
+                  Spacer(modifier = Modifier.width(24.dp))
+                }
+              }
+        }
+  }
+}
