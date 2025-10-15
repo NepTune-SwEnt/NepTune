085aceae9994fbc12aab68bd5d7c495547eebbc2
abaur3 <arianna.baur@epfl.ch>
2025-10-15 17:22:32 +0200

    chore(profile): add Repositories singleton with ProfileRepositoryFirebase

---
+++ app/src/main/java/com/neptune/neptune/model/Repositories.kt
@@ -0,0 +1,11 @@
+package com.neptune.neptune.model
+
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.storage
+
+object Repositories {
+    val profile by lazy {
+        ProfileRepositoryFirebase(Firebase.firestore, Firebase.storage)
+    }
+}
