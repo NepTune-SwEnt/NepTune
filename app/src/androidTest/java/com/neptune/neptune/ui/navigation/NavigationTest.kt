88f9fa1e367fd2c8227c07995ff5496c68a5e274
touunyy <tony.andriamampianina@epfl.ch>
2025-10-17 17:06:22 +0200

    feat(test): add assertTextEquals import to NavigationTest

--- app/src/androidTest/java/com/neptune/neptune/ui/navigation/NavigationTest.kt
+++ app/src/androidTest/java/com/neptune/neptune/ui/navigation/NavigationTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertIsNotSelected
 import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
