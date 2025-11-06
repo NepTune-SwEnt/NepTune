86508093fbd239346f97a384dadf4df2f8918c3b
greg <gregory.blanc@epfl.ch>
2025-11-06 20:51:10 +0100

    feat: Add uCrop and Coil dependencies
    This commit introduces two new dependencies to the project:
    
    - `uCrop`: An image cropping library from Yalantis, added to enable image manipulation features.
    - `Coil`: An image loading library for Android, used for asynchronously handling images in composables.
    
    An `UCropActivity` with a portrait screen orientation has also been added to the `AndroidManifest.xml` to support the new cropping functionality.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -199,6 +199,10 @@ dependencies {
     testImplementation("app.cash.turbine:turbine:0.12.3")
     androidTestImplementation("app.cash.turbine:turbine:0.12.3")
 
+    implementation("com.github.yalantis:ucrop:2.2.8")
+
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
 }
 
 tasks.withType<Test> {
