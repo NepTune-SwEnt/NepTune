bba0d04136d12bf3bc650bae4eafc2ed6924a627
greg <gregory.blanc@epfl.ch>
2025-11-12 21:02:51 +0100

    docs: Add notice about AI assistance in StorageServiceTest
    Adds a comment to the `StorageServiceTest.kt` file to indicate that the class was created with the help of AI.

--- app/src/androidTest/java/com/neptune/neptune/data/storage/StorageServiceTest.kt
+++ app/src/androidTest/java/com/neptune/neptune/data/storage/StorageServiceTest.kt
@@ -22,6 +22,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+// This class was made using AI assistance.
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class StorageServiceTest {
