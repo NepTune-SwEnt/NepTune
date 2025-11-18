30c8b28464a53bd281e19f4a24ed6c3035baeebf
abaur3 <arianna.baur@epfl.ch>
2025-11-18 14:25:21 +0100

    feat(profile): add an enum class for selecting the profile view needed in a particular situation
    The enum class contains two different types:
    - SELF: used when viewing the currently signed-in user’s profile
    - OTHER_USER: used when viewing another user’s profile

---
+++ app/src/main/java/com/neptune/neptune/ui/profile/ProfileViewType.kt
@@ -0,0 +1,9 @@
+package com.neptune.neptune.ui.profile
+
+/** Represents the type of profile view:
+ * self: for viewing the currently signed-in user's profile, or
+ * other user: for viewing another user's profile */
+enum class ProfileViewType {
+    SELF,
+    OTHER_USER
+}
