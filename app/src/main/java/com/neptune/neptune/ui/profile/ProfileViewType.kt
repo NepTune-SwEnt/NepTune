3ba66053d2db608c7e75e388fb81b36e8e78cc57
abaur3 <arianna.baur@epfl.ch>
2025-11-18 16:34:07 +0100

    refactor: reformat code with ktfmt

--- app/src/main/java/com/neptune/neptune/ui/profile/ProfileViewType.kt
+++ app/src/main/java/com/neptune/neptune/ui/profile/ProfileViewType.kt
@@ -1,9 +1,10 @@
 package com.neptune.neptune.ui.profile
 
-/** Represents the type of profile view:
- * self: for viewing the currently signed-in user's profile, or
- * other user: for viewing another user's profile */
+/**
+ * Represents the type of profile view: self: for viewing the currently signed-in user's profile, or
+ * other user: for viewing another user's profile
+ */
 enum class ProfileViewType {
-    SELF,
-    OTHER_USER
+  SELF,
+  OTHER_USER
 }
