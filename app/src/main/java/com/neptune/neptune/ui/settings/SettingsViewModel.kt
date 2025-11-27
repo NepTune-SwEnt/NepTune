6186b03d1e14c0513582639410aed2d31d04ecbc
Tim <timeo.golaz@epfl.ch>
2025-11-27 23:21:00 +0100

    fix: remove unnecessary parameter

--- app/src/main/java/com/neptune/neptune/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/neptune/neptune/ui/settings/SettingsViewModel.kt
@@ -52,9 +52,7 @@ class SettingsViewModel(private val themeDataStore: ThemeDataStore) : ViewModel(
   }
 
   fun updateCustomColors(primary: Color, background: Color, onBackground: Color) {
-    viewModelScope.launch {
-      themeDataStore.setCustomColors(primary, background, onBackground, onBackground)
-    }
+    viewModelScope.launch { themeDataStore.setCustomColors(primary, background, onBackground) }
   }
 
   fun resetCustomColors() {
