06961a9d05d1f434e0f223ea4f832b2641908c07
abaur3 <arianna.baur@epfl.ch>
2025-12-04 13:03:31 +0100

    refactor(search): refactor SearchScreen feed to use shared SampleFeedItems
    - Revert ScrollableColumnOfSamples controller parameter to SearchViewModel in SearchScreenAllTest
    - Introduce SampleFeedItems helper to render shared sample cards for search and profile
    - Update SearchScreen.ScrollableColumnOfSamples to call sampleFeedItems instead of inlining item creation
    - Add testTagsForSample parameter to  ScrollableColumnOfSamples so tags can be passed to SampleFeedItems
    - Move spacing between samples into SampleFeedItems and remove verticalArrangement spacing in the LazyColumn
    - Drop broken isAnonymous handling introduced by a bad rebase; it will be re-added in a proper merge fix

--- app/src/test/java/com/neptune/neptune/ui/SearchScreenAllTests.kt
+++ app/src/test/java/com/neptune/neptune/ui/SearchScreenAllTests.kt
@@ -21,8 +21,8 @@ import com.neptune.neptune.model.fakes.FakeSampleRepository
 import com.neptune.neptune.model.profile.ProfileRepository
 import com.neptune.neptune.model.sample.Sample
 import com.neptune.neptune.model.sample.SampleRepository
-import com.neptune.neptune.ui.feed.ScrollableColumnOfSamples
 import com.neptune.neptune.ui.projectlist.ProjectListScreenTestTags
+import com.neptune.neptune.ui.search.ScrollableColumnOfSamples
 import com.neptune.neptune.ui.search.SearchScreen
 import com.neptune.neptune.ui.search.SearchScreenTestTags
 import com.neptune.neptune.ui.search.SearchScreenTestTags.SAMPLE_LIST
@@ -296,7 +296,7 @@ class SearchScreenAllTests {
     composeRule.setContent {
       ScrollableColumnOfSamples(
           samples = listOf(sample),
-          controller = vm,
+          searchViewModel = vm,
           mediaPlayer = fakeMediaPlayer,
           testTagsForSample = { SearchScreenTestTagsPerSampleCard(it.id) },
           navigateToOtherUserProfile = { navigatedTo = it })
@@ -340,7 +340,7 @@ class SearchScreenAllTests {
     composeRule.setContent {
       ScrollableColumnOfSamples(
           samples = listOf(sample),
-          controller = vm,
+          searchViewModel = vm,
           mediaPlayer = fakeMediaPlayer,
           testTagsForSample = { SearchScreenTestTagsPerSampleCard(it.id) },
           navigateToProfile = { navigatedToSelf = true },
