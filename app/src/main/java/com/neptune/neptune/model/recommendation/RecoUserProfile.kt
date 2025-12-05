0d36279c6a6ad973d0e3ec40cf28a77b7cfb3a0c
touunyy <tony.andriamampianina@epfl.ch>
2025-12-05 02:14:07 +0100

    refactor: remove empty body from RecoUserProfile data class
    Removes the unnecessary empty curly braces from the `RecoUserProfile` data class definition, making the code more concise.

--- app/src/main/java/com/neptune/neptune/model/recommendation/RecoUserProfile.kt
+++ app/src/main/java/com/neptune/neptune/model/recommendation/RecoUserProfile.kt
@@ -1,3 +1,3 @@
 package com.neptune.neptune.model.recommendation
 
-data class RecoUserProfile(val uid: String, val tagsWeight: Map<String, Double>) {}
+data class RecoUserProfile(val uid: String, val tagsWeight: Map<String, Double>)
