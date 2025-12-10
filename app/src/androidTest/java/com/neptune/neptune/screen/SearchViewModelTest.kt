c452dd85221a186f065ba4bff67db0dc33f550c9
greg <gregory.blanc@epfl.ch>
2025-12-10 23:47:16 +0100

    chore: fix indentation in SearchViewModelTest
    This commit corrects the indentation of the `auth.signInAnonymously().await()` call within the `integrationLoadSamplesAndIncrementalUpdate` test in `SearchViewModelTest.kt`.

--- app/src/androidTest/java/com/neptune/neptune/screen/SearchViewModelTest.kt
+++ app/src/androidTest/java/com/neptune/neptune/screen/SearchViewModelTest.kt
@@ -59,7 +59,7 @@ class SearchViewModelTest {
 
   @Test
   fun integrationLoadSamplesAndIncrementalUpdate() = runBlocking {
-      auth.signInAnonymously().await()
+    auth.signInAnonymously().await()
     clearFirestoreSamples()
 
     val audioPath = "test_samples/audio_${UUID.randomUUID()}.mp3"
