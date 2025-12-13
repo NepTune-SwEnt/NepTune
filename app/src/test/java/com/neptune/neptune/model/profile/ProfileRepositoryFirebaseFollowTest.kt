7867c1d45c5583b9617d8de14263d7a0e45d95a3
abaur3 <arianna.baur@epfl.ch>
2025-12-13 20:45:50 +0100

    test(follow-list): add followers/followingIds tests based on subcollections
    - cover getFollowingIds/getFollowersIds using mocked follow subcollections
    - align stubs with Firestore structure to avoid missing answer errors

--- app/src/test/java/com/neptune/neptune/model/profile/ProfileRepositoryFirebaseFollowTest.kt
+++ app/src/test/java/com/neptune/neptune/model/profile/ProfileRepositoryFirebaseFollowTest.kt
@@ -3,7 +3,12 @@ package com.neptune.neptune.model.profile
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.functions.HttpsCallableReference
 import com.google.firebase.functions.HttpsCallableResult
@@ -85,6 +90,59 @@ class ProfileRepositoryFirebaseFollowTest {
         Tasks.forException(IllegalStateException("firestore down"))
 
     runBlocking { assertFailsWith<IllegalStateException> { repo.followUser("abc") } }
+  }
+
+  @Test
+  fun getFollowingIdsReturnsListFromFirestore() = runBlocking {
+    val expectedIds = listOf("id1", "id2", "id3")
+    val docRef = mockk<DocumentReference>()
+    val collection = mockk<CollectionReference>()
+    val querySnapshot = mockk<QuerySnapshot>()
+    val doc1 = mockk<QueryDocumentSnapshot>()
+    val doc2 = mockk<QueryDocumentSnapshot>()
+    val doc3 = mockk<QueryDocumentSnapshot>()
+
+    every { firestore.collection("profiles").document(CURRENT_UID) } returns docRef
+    every { docRef.collection("following") } returns collection
+    every { collection.get() } returns Tasks.forResult(querySnapshot)
+    every { querySnapshot.documents } returns listOf(doc1, doc2, doc3)
+    every { doc1.id } returns "id1"
+    every { doc1.getString("uid") } returns null
+    every { doc2.id } returns "id2"
+    every { doc2.getString("uid") } returns null
+    every { doc3.id } returns "id3"
+    every { doc3.getString("uid") } returns null
+
+    val followingIds = repo.getFollowingIds(uid = CURRENT_UID)
+
+    assertEquals(expectedIds, followingIds)
+    verify(exactly = 1) { firestore.collection("profiles").document(CURRENT_UID) }
+    verify(exactly = 1) { docRef.collection("following") }
+  }
+
+  @Test
+  fun getFollowersIdsReturnsListFromFirestore() = runBlocking {
+    val expectedIds = listOf("idA", "idB")
+    val docRef = mockk<DocumentReference>()
+    val collection = mockk<CollectionReference>()
+    val querySnapshot = mockk<QuerySnapshot>()
+    val docA = mockk<QueryDocumentSnapshot>()
+    val docB = mockk<QueryDocumentSnapshot>()
+
+    every { firestore.collection("profiles").document(CURRENT_UID) } returns docRef
+    every { docRef.collection("followers") } returns collection
+    every { collection.get() } returns Tasks.forResult(querySnapshot)
+    every { querySnapshot.documents } returns listOf(docA, docB)
+    every { docA.id } returns "idA"
+    every { docA.getString("uid") } returns null
+    every { docB.id } returns "idB"
+    every { docB.getString("uid") } returns null
+
+    val followerIds = repo.getFollowersIds(uid = CURRENT_UID)
+
+    assertEquals(expectedIds, followerIds)
+    verify(exactly = 1) { firestore.collection("profiles").document(CURRENT_UID) }
+    verify(exactly = 1) { docRef.collection("followers") }
   }
 
   companion object {
