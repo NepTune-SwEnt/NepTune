273ffbfb1e0337e482a17467f9a563e308976ae4
Uri Jaquet <urijaquet@gmail.com>
2025-12-15 00:41:03 +0100

    style: increase Home tab icon size
    This commit increases the size of the "Home" icon in the bottom navigation bar.
    
    - **`BottomNavigationMenu.kt`**: Changed the `iconSize` for `Tab.Main` from `20.dp` to `32.dp` for better visual prominence. Minor code reformatting was also applied.

--- app/src/main/java/com/neptune/neptune/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/neptune/neptune/ui/navigation/BottomNavigationMenu.kt
@@ -28,48 +28,58 @@ import com.neptune.neptune.ui.theme.NepTuneTheme
  * @param destination The destination screen of the tab
  * @param testTag The test tag of the tab
  */
-sealed class Tab(val name: String, val icon: Int, val destination: Screen, val testTag: String, val iconSize: Dp = 25.dp ) {
+sealed class Tab(
+  val name: String,
+  val icon: Int,
+  val destination: Screen,
+  val testTag: String,
+  val iconSize: Dp = 25.dp
+) {
 
   object Main : Tab(
     "Home",
     R.drawable.home_planet,
     Screen.Main,
     NavigationTestTags.MAIN_TAB,
-    iconSize = 20.dp)
+    iconSize = 32.dp
+  )
 
   object ProjectList :
-      Tab(
-          "Select Project",
-          // Credits:
-          // https://www.flaticon.com/free-icon-font/list-music_10742896
-          R.drawable.list_projects_unselected,
-          Screen.ProjectList,
-          NavigationTestTags.PROJECTLIST_TAB)
+    Tab(
+      "Select Project",
+      // Credits:
+      // https://www.flaticon.com/free-icon-font/list-music_10742896
+      R.drawable.list_projects_unselected,
+      Screen.ProjectList,
+      NavigationTestTags.PROJECTLIST_TAB
+    )
 
   object Search :
-      Tab(
-          "Search",
-          // Credits:
-          // https://www.flaticon.com/free-icon-font/search_3917754
-          R.drawable.search_unselected,
-          Screen.Search,
-          NavigationTestTags.SEARCH_TAB)
+    Tab(
+      "Search",
+      // Credits:
+      // https://www.flaticon.com/free-icon-font/search_3917754
+      R.drawable.search_unselected,
+      Screen.Search,
+      NavigationTestTags.SEARCH_TAB
+    )
 
   object Messages :
     Tab(
       "Messages",
       R.drawable.messageicon,
       Screen.SelectMessages,
-      NavigationTestTags.MESSAGE_BUTTON)
+      NavigationTestTags.MESSAGE_BUTTON
+    )
 }
 
 private val tabs =
-    listOf(
-        Tab.Main,
-        Tab.Search,
-        Tab.ProjectList,
-        Tab.Messages,
-    )
+  listOf(
+    Tab.Main,
+    Tab.Search,
+    Tab.ProjectList,
+    Tab.Messages,
+  )
 
 /**
  * Get the tab for the given route.
@@ -83,62 +93,68 @@ fun getTabForRoute(route: String?): Tab? {
 
 @Composable
 fun BottomNavigationMenu(
-    screen: Screen = Screen.Main,
-    navigationActions: NavigationActions? = null,
-    currentScreenArguments: Bundle? = null
+  screen: Screen = Screen.Main,
+  navigationActions: NavigationActions? = null,
+  currentScreenArguments: Bundle? = null
 ) {
   if (!screen.showBottomBar) {
     return
   }
   Column {
     HorizontalDivider(
-        modifier = Modifier.fillMaxWidth(),
-        thickness = 0.75.dp,
-        color = NepTuneTheme.colors.onBackground)
+      modifier = Modifier.fillMaxWidth(),
+      thickness = 0.75.dp,
+      color = NepTuneTheme.colors.onBackground
+    )
     NavigationBar(
-        modifier = Modifier
-          .testTag(NavigationTestTags.BOTTOM_NAVIGATION_MENU)
-          .height(55.dp)
-          .padding(horizontal = 7.dp),
-        containerColor = NepTuneTheme.colors.background) {
-          tabs.forEach { tab ->
-            val isSelected: Boolean =
-                when (tab.destination) {
-                  Screen.ProjectList -> {
-                    val onProjectListScreen = (screen == Screen.ProjectList)
-                    val purposeIsEdit = (currentScreenArguments?.getString("purpose") == "edit")
-                    onProjectListScreen && purposeIsEdit
-                  }
-                  else -> {
-                    tab.destination.route == screen.route
-                  }
-                }
-            NavigationBarItem(
-                icon = {
-                  Icon(
-                    painter = painterResource(id = tab.icon),
-                    contentDescription = tab.name,
-                    modifier = Modifier.size(tab.iconSize),
-                    tint = NepTuneTheme.colors.onBackground)
-                },
-              alwaysShowLabel = false,
-                selected = isSelected,
-                onClick = {
-                  if (tab.destination == Screen.ProjectList) {
-                    navigationActions?.navigateTo(Screen.ProjectList.createRoute("edit"))
-                  } else {
-                    navigationActions?.navigateTo(tab.destination)
-                  }
-                },
-                enabled = navigationActions != null,
-                modifier = Modifier.testTag(tab.testTag),
-                colors =
-                    NavigationBarItemDefaults.colors(
-                        selectedTextColor = NepTuneTheme.colors.accentPrimary,
-                        selectedIconColor = NepTuneTheme.colors.accentPrimary,
-                        unselectedIconColor = NepTuneTheme.colors.onBackground,
-                        indicatorColor = NepTuneTheme.colors.indicatorColor))
+      modifier = Modifier
+        .testTag(NavigationTestTags.BOTTOM_NAVIGATION_MENU)
+        .height(55.dp)
+        .padding(horizontal = 7.dp),
+      containerColor = NepTuneTheme.colors.background
+    ) {
+      tabs.forEach { tab ->
+        val isSelected: Boolean =
+          when (tab.destination) {
+            Screen.ProjectList -> {
+              val onProjectListScreen = (screen == Screen.ProjectList)
+              val purposeIsEdit = (currentScreenArguments?.getString("purpose") == "edit")
+              onProjectListScreen && purposeIsEdit
+            }
+
+            else -> {
+              tab.destination.route == screen.route
+            }
           }
-        }
+        NavigationBarItem(
+          icon = {
+            Icon(
+              painter = painterResource(id = tab.icon),
+              contentDescription = tab.name,
+              modifier = Modifier.size(tab.iconSize),
+              tint = NepTuneTheme.colors.onBackground
+            )
+          },
+          alwaysShowLabel = false,
+          selected = isSelected,
+          onClick = {
+            if (tab.destination == Screen.ProjectList) {
+              navigationActions?.navigateTo(Screen.ProjectList.createRoute("edit"))
+            } else {
+              navigationActions?.navigateTo(tab.destination)
+            }
+          },
+          enabled = navigationActions != null,
+          modifier = Modifier.testTag(tab.testTag),
+          colors =
+            NavigationBarItemDefaults.colors(
+              selectedTextColor = NepTuneTheme.colors.accentPrimary,
+              selectedIconColor = NepTuneTheme.colors.accentPrimary,
+              unselectedIconColor = NepTuneTheme.colors.onBackground,
+              indicatorColor = NepTuneTheme.colors.indicatorColor
+            )
+        )
+      }
+    }
   }
 }
