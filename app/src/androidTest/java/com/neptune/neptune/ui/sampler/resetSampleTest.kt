cd42b843d0f4395cc188cc2428ba250cbc439f51
Ryukriss-f <adrien.jangal@epfl.ch>
2025-12-18 05:26:02 +0100

    test(sampler/ui): remove settingsDialogSaveUpdatesInputTempoAndPitch that is no more relevant because we don't have pitch selection in input anymore

--- app/src/androidTest/java/com/neptune/neptune/ui/sampler/resetSampleTest.kt
+++ app/src/androidTest/java/com/neptune/neptune/ui/sampler/resetSampleTest.kt
@@ -1,12 +1,10 @@
 package com.neptune.neptune.ui.sampler
 
-import android.net.Uri
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import com.neptune.neptune.NepTuneApplication
 import io.mockk.Runs
 import io.mockk.coEvery
-import io.mockk.coVerify
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.spyk
@@ -20,30 +18,29 @@ import org.junit.Test
 @OptIn(ExperimentalCoroutinesApi::class)
 class SamplerViewModelTest {
 
-    private lateinit var viewModel: SamplerViewModel
+  private lateinit var viewModel: SamplerViewModel
 
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
+  @get:Rule val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Before
-    fun setup() {
-        NepTuneApplication.appContext = ApplicationProvider.getApplicationContext()
-        viewModel = SamplerViewModel()
-    }
+  @Before
+  fun setup() {
+    NepTuneApplication.appContext = ApplicationProvider.getApplicationContext()
+    viewModel = SamplerViewModel()
+  }
 
-    @Test
-    fun resetSampleAndSave_resetsStateAndSaves() = runTest {
-        val zipPath = "fake/path.zip"
-        val viewModel = spyk(SamplerViewModel())
+  @Test
+  fun resetSampleAndSave_resetsStateAndSaves() = runTest {
+    val zipPath = "fake/path.zip"
+    val viewModel = spyk(SamplerViewModel())
 
-        coEvery { viewModel.stopPreview() } just Runs
-        coEvery { viewModel.saveProjectData(zipPath) } returns mockk()
+    coEvery { viewModel.stopPreview() } just Runs
+    coEvery { viewModel.saveProjectData(zipPath) } returns mockk()
 
-        viewModel.resetSampleAndSave(zipPath)
+    viewModel.resetSampleAndSave(zipPath)
 
-        val state = viewModel.uiState.value
-        assertEquals(false, state.isPlaying)
-        assertEquals(false, state.previewPlaying)
-        assertEquals(state.inputTempo, state.tempo)
-    }
+    val state = viewModel.uiState.value
+    assertEquals(false, state.isPlaying)
+    assertEquals(false, state.previewPlaying)
+    assertEquals(state.inputTempo, state.tempo)
+  }
 }
